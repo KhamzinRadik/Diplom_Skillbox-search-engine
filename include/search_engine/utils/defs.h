9b307da0a124c12c5d4cc2c17aeecb7275d1bf7b
Радик <khamzin.radik.87@mail.ru>
2024-09-25 23:41:20 +0500

    '!'

--- include/search_engine/utils/defs.h
+++ include/search_engine/utils/defs.h
@@ -5,6 +5,6 @@
 
 const int LINE_LENGTH = 46;
 const int HEADER_SPACER = 15;
-const std::string ANSWERS_FILE_PATH = "../bin/answers.json";
-const std::string CONFIGS_DIR = "../bin/configs.json";
-const std::string REQUESTS_DIR = "../bin/requests.json";
+const std::string ANSWERS_FILE_PATH = "bin/answers.json";
+const std::string CONFIGS_DIR ="bin/configs.json";
+const std::string REQUESTS_DIR = "bin/requests.json";
